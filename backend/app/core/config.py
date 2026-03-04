91c9b13d990a0bca2deec9b5a228c6e80192331c
Ammari Majd <amerimajd0@gmail.com>
2026-03-04 14:22:38 +0100

    add froget_pass&reset_pass endpoints&pass_refresh_token_table to DB

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,11 +1,22 @@
 from pydantic_settings import BaseSettings
+
+
 class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
     ADMIN_SIGNUP_KEY: str
+
+    SMTP_HOST: str = "sandbox.smtp.mailtrap.io"
+    SMTP_PORT: int = 587
+    SMTP_USER: str = ""
+    SMTP_PASS: str = ""
+    SMTP_FROM: str = "noreply@3dp.com"
+    APP_BASE_URL: str = "http://localhost:8000"
+
     class Config:
         env_file = ".env"
-    
+
+
 settings = Settings()
