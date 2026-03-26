24ffe496fdd14b501339d5a36e9a4a4e805d4068
Ammari Majd <amerimajd0@gmail.com>
2026-03-26 14:27:18 +0100

    add splash screen

--- frontend/lib/core/router/app_routes.dart
+++ frontend/lib/core/router/app_routes.dart
@@ -5,20 +5,19 @@ class AppRoutes {
 
   // ── Auth ──
   static const String adminSignup = '/admin-signup';
-  static const String inviteUser  = '/invite';
-  static const String register    = '/register';
-  static const String login    = '/login';
-  static const String forgotPassword ='/forgot-password';
+  static const String inviteUser = '/invite';
+  static const String register = '/register';
+  static const String login = '/login';
+  static const String forgotPassword = '/forgot-password';
   static const String resetPassword = '/reset-password';
 
   // ── Dashboard (à venir) ──
-  static const String dashboard   = '/dashboard';
+  static const String dashboard = '/dashboard';
 
   static const String upload = '/upload';
   static const String fileDetail = '/upload/file';
   static const String fleet = '/fleet';
   static const String schedule = '/schedule';
   static const String monitoring = '/monitoring';
-  
-
+  static const String splash = '/splash';
 }
